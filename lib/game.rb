da99d4ea7e1552ed4279eee48b46b38dcfad7fe0
Edward Withers <edward.withers@smartpension.co.uk>
2020-11-20 12:40:39 +0000

    refactor: extract methods

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ class Game
   MIN_PTS_TO_WIN = 4
   WIN_BY_AMOUNT = 2
 
-  def initialize()
+  def initialize
     @points = []
   end
 
@@ -14,23 +14,28 @@ class Game
   end
 
   def score
-    server_points = @points.count(:server)
-    returner_points = @points.count(:returner)
-    
-    current_max_role = @points.max_by { |role| @points.count(role) }
-    current_max_pts = [server_points, returner_points].max
+    server_pts = @points.count(:server)
+    returner_pts = @points.count(:returner)
 
-    if current_max_pts >= MIN_PTS_TO_WIN && (server_points - returner_points).abs >= WIN_BY_AMOUNT
-      "game won by #{current_max_role}"
-    else
-      score_for_both(server_points, returner_points)
-    end
+    current_max_pts = [server_pts, returner_pts].max
+
+    return "game won by #{points_leader}" if game_won?(server_pts, returner_pts, current_max_pts)
+
+    score_for_both(server_pts, returner_pts)
   end
 
   private
 
+  def points_leader
+    @points.max_by { |role| @points.count(role) }
+  end
+
+  def game_won?(server_points, returner_points, current_max_pts)
+    current_max_pts >= MIN_PTS_TO_WIN && (server_points - returner_points).abs >= WIN_BY_AMOUNT
+  end
+
   def score_for_both(server_points, returner_points)
-    if server_points + returner_points >= 6
+    if deuce_game?(server_points, returner_points)
       if server_points == returner_points
         "#{DEUCE_SCORES[0]}:#{DEUCE_SCORES[0]}"
       elsif server_points > returner_points
@@ -41,5 +46,9 @@ class Game
     else
       "#{SCORES[server_points]}:#{SCORES[returner_points]}"
     end
+  end
+
+  def deuce_game?(server_points, returner_points)
+    server_points + returner_points >= 6
   end
 end

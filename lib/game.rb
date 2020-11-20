efd4ee297253459990a3c3a21f11409365106d19
Edward Withers <edward.withers@smartpension.co.uk>
2020-11-20 13:36:56 +0000

    refactor: store instance vars

--- lib/game.rb
+++ lib/game.rb
@@ -19,44 +19,42 @@ class Game
   end
 
   def score
-    server_pts = @points.count(:server)
-    returner_pts = @points.count(:returner)
+    @server_points = @points.count(:server)
+    @returner_points = @points.count(:returner)
 
-    return "game won by #{points_leader}" if game_won?(server_pts, returner_pts)
+    return "game won by #{points_leader}" if game_won?
 
-    format(*scores(server_pts, returner_pts))
+    format(*scores)
   end
 
   private
+
+  attr_reader :server_points, :returner_points
 
   def points_leader
     @points.max_by { |role| @points.count(role) }
   end
 
-  def game_won?(server_points, returner_points)
+  def game_won?
     current_max_pts = [server_points, returner_points].max
     current_max_pts >= MIN_PTS_TO_WIN && (server_points - returner_points).abs >= WIN_BY_AMOUNT
   end
 
-  def scores(server_points, returner_points)
-    if deuce_game?(server_points, returner_points)
-      deuce_scores(server_points, returner_points)
-    else
-      game_scores(server_points, returner_points)
-    end
+  def scores
+    deuce_game? ? deuce_scores : game_scores
   end
 
-  def deuce_game?(server_points, returner_points)
+  def deuce_game?
     server_points + returner_points >= 6
   end
 
-  def deuce_scores(server_points, returner_points)
+  def deuce_scores
     deuce_result = server_points - returner_points
     server_points, returner_points = DEUCE_RESULT[deuce_result]
     [DEUCE_SCORES[server_points], DEUCE_SCORES[returner_points]]
   end
 
-  def game_scores(server_points, returner_points)
+  def game_scores
     [SCORES[server_points], SCORES[returner_points]]
   end
 
